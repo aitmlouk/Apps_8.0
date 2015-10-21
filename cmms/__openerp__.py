42f214e19dbf2f8ad789fd7eca4f3e4303328b3e
aitmlouk <aitmlouk@gmail.com>
2015-10-21 16:14:20 +0100

    Check the Error on access to Fill a Checklist #1

--- cmms/__openerp__.py
+++ cmms/__openerp__.py
@@ -24,7 +24,7 @@
     "version": "1.0",
     "depends": ["base", "product", "stock", "account", "mail"],
     "author": "Ait-Mlouk Addi",
-    'website': 'http://odoo-services.esy.es/',
+    'website': 'http://www.aitmlouk-addi.info/',
     'sequence':1,
     "category": "Specific Modules/CMMS",
     'icon': '/cmms/static/src/img/icon.png',
@@ -42,7 +42,7 @@ With this module you can following all equipment type.
 For more information:
 
  - Gmail: aitmlouk@gmail.com
- - website : http://odoo-services.esy.es/
+ - website : http://www.aitmlouk-addi.info/
 
 """,
 
